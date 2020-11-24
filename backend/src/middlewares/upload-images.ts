40225680c27eaf607479b6e5e705e217b3922cec
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-24 13:47:55 -0300

    refactor: folders and repositories migration

--- backend/src/middlewares/upload-images.ts
+++
@@ -1,6 +0,0 @@
-import multer from 'multer';
-import uploadConfig from '../config/upload';
-
-const upload = multer(uploadConfig);
-
-export default upload;
--- backend/src/routes/search.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from 'express';
-import SearchController from '../controllers/SearchController';
-const routes = Router();
-
-routes.get('/search', SearchController.index);
-
-export default routes
