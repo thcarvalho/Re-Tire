fa02dccc47bf576a5dd586b503bd12b8c650c012
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-12-28 15:35:05 -0300

    feat: update baseRUL for use dotenv

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { REACT_APP_API_IP } from '@env';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.3:3333',
+  baseURL: REACT_APP_API_IP,
 });
 
 export default api;
